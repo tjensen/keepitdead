46d14d468677f169ed111e96c2169085faf3c4d4
Tim Jensen <tim.l.jensen@gmail.com>
2024-10-26 09:09:09 -0500

    Preventing extra zombies from being spawned on player death when InediaInfectedAI is also loaded on the server

--- ProjetosBR_HumanZombie/scripts/4_World/dayzplayerimplement.c
+++ ProjetosBR_HumanZombie/scripts/4_World/dayzplayerimplement.c
@@ -1,23 +1,40 @@
+#ifdef SERVER
 modded class DayZPlayerImplement
 {
+    private ZombieBase Keepitdead_zombie;
+
     override void EEKilled(Object killer)
     {
         super.EEKilled(killer);
-        if (g_Game.GetPBHZConfig().Get_SpawnZombieOnPlayersDeath())
+
+        if (!g_Game.GetPBHZConfig().Get_SpawnZombieOnPlayersDeath())
         {
-            vector zombieOffset = GetPosition() - "0.3 -0.3 0.3";
-
-            ZombieBase humanZ2;
-
-            ref array<string> Hz_ZombieClass = g_Game.GetPBHZConfig().Get_ZombieClassName();
-            humanZ2 = ZombieBase.Cast(GetGame().CreateObjectEx(Hz_ZombieClass.GetRandomElement(), zombieOffset, ECE_PLACE_ON_SURFACE | ECE_INITAI));
-
-            humanZ2.TransferAttachmentsFromZombie(this);
-            humanZ2.SetOrientation(GetOrientation());
-
-            GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(Delete, 2500);
+            return;
         }
+
+        if (Keepitdead_zombie)
+        {
+            PrintFormat("Dead player %1 already spawned zombie; skipping", this);
+            return;
+        }
+
+        PrintFormat("Player %1 has died; spawning zombie", this);
+
+        vector zombieOffset = GetPosition() - "0.3 -0.3 0.3";
+
+        ref array<string> Hz_ZombieClass = g_Game.GetPBHZConfig().Get_ZombieClassName();
+        Keepitdead_zombie = ZombieBase.Cast(
+            GetGame().CreateObjectEx(
+                Hz_ZombieClass.GetRandomElement(),
+                zombieOffset,
+                ECE_PLACE_ON_SURFACE | ECE_INITAI));
+
+        Keepitdead_zombie.TransferAttachmentsFromZombie(this);
+        Keepitdead_zombie.SetOrientation(GetOrientation());
+
+        GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(Delete, 2500);
     }
 };
+#endif // SERVER
 
 // vim:ft=enforce
