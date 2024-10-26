3493d8553307ee4c2b9f904faf2856a0b2a1729b
Tim Jensen <tim.l.jensen@gmail.com>
2024-10-26 09:52:32 -0500

    Renaming PBO file to more closely match the mod name

--- build.ps1
+++ build.ps1
@@ -1,4 +1,5 @@
 $ModName = "keepitdead"
+$Prefix = "ProjetosBR_HumanZombie"
 
 $ErrorActionPreference = "Stop"
 
@@ -8,10 +9,13 @@ New-Item -Type Directory "P:\@${ModName}" | Out-Null
 New-Item -Type Directory "P:\@${ModName}\addons" | Out-Null
 New-Item -Type Directory "P:\@${ModName}\keys" | Out-Null
 
-& "$((Get-ItemProperty -Path "HKCU:\Software\bohemia interactive\Dayz Tools").path)\Bin\AddonBuilder\AddonBuilder.exe" "P:\keepitdead\ProjetosBR_HumanZombie" "P:\@${ModName}\addons" "-clear" "-project=P:\keepitdead\ProjetosBR_HumanZombie" "-sign=$env:BIPRIVATEKEY_PATH" "-packonly"
+& "$((Get-ItemProperty -Path "HKCU:\Software\bohemia interactive\Dayz Tools").path)\Bin\AddonBuilder\AddonBuilder.exe" "P:\keepitdead\${Prefix}" "P:\@${ModName}\addons" "-clear" "-project=P:\keepitdead\${Prefix}" "-sign=$env:BIPRIVATEKEY_PATH" "-packonly"
 $buildcode = $lastexitcode
 $builderror = $errorMessage
 
 if ($buildcode -ne 0) { throw ("Exec: " + $builderror) }
+
+Move-Item "P:\@${ModName}\addons\${Prefix}.pbo" "P:\@${ModName}\addons\${ModName}.pbo"
+Move-Item "P:\@${ModName}\addons\${Prefix}.pbo.crimsonzamboni.bisign" "P:\@${ModName}\addons\${ModName}.pbo.crimsonzamboni.bisign"
 
 Copy-Item $env:BIKEY_PATH -Destination "P:\@${ModName}\keys"
