0c984cb67af9c65e39f1be1cf32b952fd015e807
Tim Jensen <tim.l.jensen@gmail.com>
2024-10-26 09:33:07 -0500

    Removing unnecessary support for client-side zombie spawning because that code should only ever run on the server when running in a true multiplayer environment, like CHF.

--- ProjetosBR_HumanZombie/scripts/4_World/zombiebase.c
+++ ProjetosBR_HumanZombie/scripts/4_World/zombiebase.c
@@ -5,17 +5,9 @@
 
 modded class ZombieBase : DayZInfected
 {
-    protected bool m_ReceivedAttachments;
-
-    void HumanZombie_Base()
-    {
-        RegisterNetSyncVariableBool("m_ReceivedAttachments");
-    }
-
+#ifdef SERVER
     void TransferAttachmentsFromZombie(EntityAI entity)
     {
-        if (g_Game.GetPBHZConfig().Get_SpawnZombieOnPlayersDeath())
-        {
         for (int i = 0; i < entity.GetInventory().GetAttachmentSlotsCount(); i++)
         {
             int slot = entity.GetInventory().GetAttachmentSlotId(i);
@@ -23,57 +15,23 @@ modded class ZombieBase : DayZInfected
 
             if (item)
             {
-                float health;
-
-                if (GetGame().IsServer())
-                {
-                    health = item.GetHealth();
-                    item.SetHealth(item.GetMaxHealth());
-                }
+                float health = item.GetHealth();
+                item.SetHealth(item.GetMaxHealth());
 
                 if (GetInventory().CanAddAttachment(item))
                 {
-                    if (GetGame().IsMultiplayer())
-                        entity.ServerTakeEntityToTargetInventory(this, FindInventoryLocationType.ATTACHMENT, item);
-                    else
-                        entity.LocalTakeEntityToTargetInventory(this, FindInventoryLocationType.ATTACHMENT, item);
+                    entity.ServerTakeEntityToTargetInventory(this, FindInventoryLocationType.ATTACHMENT, item);
                 }
                 else
                 {
-                    if (GetGame().IsMultiplayer())
-                        entity.GetInventory().DropEntity(InventoryMode.SERVER, entity, item);
-                    else
-                        entity.GetInventory().DropEntity(InventoryMode.LOCAL, entity, item);
+                    entity.GetInventory().DropEntity(InventoryMode.SERVER, entity, item);
                 }
 
-                if (GetGame().IsServer())
-                    item.SetHealth(health);
+                item.SetHealth(health);
             }
         }
-
-        m_ReceivedAttachments = true;
-        SetSynchDirty();
-        }
     }
-
-    override bool HandleDeath(int pCurrentCommandID)
-    {
-        if ( pCurrentCommandID == DayZInfectedConstants.COMMANDID_DEATH )
-        {
-            return true;
-        }
-
-        if ( !IsAlive() )
-        {
-            StartCommand_Death(m_DeathType, m_DamageHitDirection);
-            m_MovementSpeed = -1;
-            m_MindState = -1;
-            SetSynchDirty();
-            return true;
-        }
-
-        return false;
-    }
+#endif // SERVER
 };
 
 // vim:ft=enforce
